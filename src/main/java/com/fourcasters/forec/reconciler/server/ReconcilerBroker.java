152b69a5aba291eaa9e0693c177ab0b38ca8abfc
ettoremaiorana <ettoremaiorana@tiscali.it>
2016-03-13 21:46:27 +0000

    Add listener to log info

--- src/main/java/com/fourcasters/forec/reconciler/server/ReconcilerBroker.java
+++ src/main/java/com/fourcasters/forec/reconciler/server/ReconcilerBroker.java
@@ -39,6 +39,7 @@ public class ReconcilerBroker {
 	private static final String HISTORY_TOPIC_NAME = "HISTORY@";
 	private static final String RECONCILER_TOPIC_NAME = "RECONC@";
 	private static final String NEW_TRADES_TOPIC_NAME = "STATUS@";
+	private static final String LOG_INFO_TOPIC_NAME = "LOG@INFO";
 
 	private static final int bufferSize = 10240;
 	private static final byte[] TOPIC_NAME_IN_INPUT = new byte[bufferSize];
@@ -180,6 +181,9 @@ public class ReconcilerBroker {
 		final Socket listener = ctx.socket(ZMQ.SUB);
 		listener.connect("tcp://localhost:50027");
 		listener.subscribe(NEW_TRADES_TOPIC_NAME.getBytes());
+		if (Boolean.getBoolean(System.getProperty("log.info"))) {
+			listener.subscribe(LOG_INFO_TOPIC_NAME.getBytes());
+		}
 		return listener;
 	}
 
