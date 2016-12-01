1cee0a550395c20f1d1da2c6e254ce1bf4fdad09
ettoremaiorana <ettoremaiorana@tiscali.it>
2016-12-01 09:44:39 +0000

    Unit test for json format http response in db history query

--- src/test/java/com/fourcasters/forec/reconciler/server/http/HistoryServletTest.java
+++ src/test/java/com/fourcasters/forec/reconciler/server/http/HistoryServletTest.java
@@ -1,6 +1,21 @@
 package com.fourcasters.forec.reconciler.server.http;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,19 +27,6 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.fourcasters.forec.reconciler.query.history.HistoryDAO;
-import com.fourcasters.forec.reconciler.server.http.HttpParser;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.channels.SocketChannel;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @RunWith(MockitoJUnitRunner.class)
 public class HistoryServletTest {
@@ -34,23 +36,28 @@ public class HistoryServletTest {
 	private static HistoryDAO dao;
 	private static String oldProp;
 	private static Socket socket;
-	private static ServerSocket server;
+	private static ServerSocketChannel server;
 	private static Thread t;
+	private static Socket client;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		oldProp = System.getProperty("ENV");
 		System.setProperty("ENV", "test");
 		dao = new HistoryDAO();
 		dao.dbhash("eurusd", "yyyy.mm.dd,HH:MM,o,h,l,c,v");
-		server = new ServerSocket(54345);
+		server = ServerSocketChannel.open();
+		server.configureBlocking(true);
+		server.socket().bind(new InetSocketAddress("localhost", 54345));
 		CountDownLatch latch = new CountDownLatch(1);
 		t = new Thread(new Runnable() {
+
 			@Override
 			public void run() {
 				try {
 					while(true) {
 						latch.countDown();
-						server.accept();
+						client = server.accept().socket();
 					}
 				} catch (IOException e) {
 				}
@@ -101,4 +108,34 @@ public class HistoryServletTest {
 		assertEquals(stop-start, result);
 	}
 
+	@Test
+	public void testJsonFormat() throws IOException, InterruptedException {
+		CountDownLatch latch = new CountDownLatch(1);
+		when(parser.getParam("format")).thenReturn("json");
+		final byte[] buff = new byte[1024*1024];
+		final ByteBuffer bb = ByteBuffer.allocate(1024*1024);
+		Thread t = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					int totRead = 0;
+					while (totRead < 100) {
+						bb.limit(bb.capacity());
+						totRead += channel.read(bb); //body
+					}
+					latch.countDown();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "Read-me");
+		t.start();
+		new HistoryServlet(parser, dao).respond(client.getChannel());
+		latch.await(10000, TimeUnit.MILLISECONDS);
+		bb.flip();
+		bb.get(buff,0,bb.limit());
+		String resp = new String(buff);
+		assertTrue(resp.contains("{\"data\":"));
+	}
 }
